f6cb6661491bbf370b83c8840f39dce8c50ed9af
AlbertoBass <alberto.bassi@lab121.org>
2018-07-24 00:58:14 +0200

    Aggiungere i file di progetto.

---
+++ Menberships/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Menberships.Startup))]
+namespace Menberships
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
